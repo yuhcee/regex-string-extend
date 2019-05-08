4d9e6b6aad42456da2af17b2a593f8e4053389ea
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-05-09 00:17:49 +0100

    Add test for String hasVowels

---
+++ assets/test/app.test.js
@@ -0,0 +1,10 @@
+require('../main/app');
+
+describe('Test if String has Vowels', function() {
+  test('should return true if string contains vowels', () => {
+    expect('Uchenna'.hasVowels()).toBeTruthy();
+  });
+  test('should return false if string does NOT contain vowels', () => {
+    expect('Rhythm'.hasVowels()).toBey();
+  });
+});
