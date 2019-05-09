b6678d297996f980e3f72788fb7685a4e67c8319
hand-Sum MUSEgit <egbouchenna001@gmail.com>
2019-05-09 10:55:55 +0100

    Add String method toUpper

--- assets/main/app.js
+++ assets/main/app.js
@@ -1,6 +1,19 @@
 String.prototype.hasVowels = function() {
-  var vowels = /[aieou]/i;
+  var vowels = /[aeiou]/i;
   return vowels.test(this);
+};
+
+String.prototype.toUpper = function() {
+  var upperCased = '';
+  for (var index = 0; index < this.length; index++) {
+    var currentValue = this.charCodeAt(index);
+    if (currentValue > 96 && currentValue < 123) {
+      upperCased += String.fromCharCode(currentValue & ~(1 << 5));
+    } else {
+      upperCased += this[index];
+    }
+  }
+  return upperCased;
 };
 
 module.exports = String;
